fe676b57c165d6e30c2711ed6c570bb168a6d90a
Amit Kaithwas <89036104+kaithwasjr@users.noreply.github.com>
2024-10-16 17:16:45 +0530

    on today

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './header/Header'
 import FormSwitch from './FormSwitch/FormSwitch'
-import SubmitBlock from './submitBlock/SubmitBlock'
 import InventoryTable from './Inventory/InventoryTable'
 
 export default function Home() {
